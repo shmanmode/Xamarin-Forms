6be6f564f097496e85c458e92ecd3ed482dbb5d0
SHUBHAM  MANMODE <sh.manmode@gmail.com>
2019-08-24 12:54:30 +0530

    Device Info In Android Using Xamarin.Forms

---
+++ Device Info/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.DeviceInfo;
+using Xamarin.Forms;
+
+namespace DeviceInfo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            InitializeComponent();
+            getDeviceInfo();
+        }
+
+        private void getDeviceInfo()
+        {
+            Id.Text = CrossDeviceInfo.Current.Id;
+
+            Idi.Text = CrossDeviceInfo.Current.Idiom.ToString();
+
+            Mdl.Text = CrossDeviceInfo.Current.Model;
+
+            PLF.Text = CrossDeviceInfo.Current.Platform.ToString();
+
+            Vs.Text = CrossDeviceInfo.Current.Version;
+        }
+    }
+}
